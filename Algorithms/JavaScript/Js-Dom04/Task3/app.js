99d2025823ba4db33301a4ca83032afc18b8c9f3
Xayyam.Gasimzada <qasimzadex@gmail.com>
2021-12-19 17:46:39 +0400

    click ile div yaratmaq

---
+++ Algorithms/JavaScript/Js-Dom04/Task3/app.js
@@ -0,0 +1,6 @@
+
+function getdata(){
+let box=document.createElement("div")
+box.className="box"
+document.body.appendChild(box)
+}
--- Algorithms/JavaScript/Js-Dom04/task2/app.js
+++ Algorithms/JavaScript/Js-Dom04/task2/app.js
@@ -6,6 +6,11 @@ function data(){
     console.log(elem)
     
     document.querySelector(".div-name").innerHTML=elem
+    document.querySelector(".div-name").style.background="red"
 
 
 }
+
+//  bu proqrqamda problem var mentorlardan xahis edirem baxanda komeklik gorsedsin. Demeli promptla metn daxil edib daha 
+//   sonra   yadda saxla vurduqda input text-de gorsedmek istirem.
+//   bunu nece ede bilerem?
