dd7bf337b6ef6bd591fa2a2777e50471bd62b71d
Xayyam.Gasimzada <qasimzadex@gmail.com>
2021-12-19 12:53:06 +0400

    Javascript bacardiqlarim

---
+++ Algorithms/JavaScript/Js-Dom04/Task1/app.js
@@ -0,0 +1,14 @@
+
+
+function data(){
+    let elem= document.querySelector(".text-name")
+    let name_txt=elem.value
+    document.querySelector(".yenielement").innerHTML=name_txt
+    document.querySelector(".yenielement").bac
+
+
+}
+
+
+
+
