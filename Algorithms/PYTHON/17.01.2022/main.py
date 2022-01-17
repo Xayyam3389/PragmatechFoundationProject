04a576e4e7710e13efdfee929dc3de4808ad69e3
Xayyam3389 <qasimzadex@gmail.com>
2022-01-17 17:57:36 +0400

    ev tapsiriqlari

---
+++ Algorithms/PYTHON/17.01.2022/main.py
@@ -0,0 +1,34 @@
+m=[1,2,3,4,5,6,7,8]
+
+# listdeki tek ededleri gorseden funksiya
+# def tekededler(n):
+#     for i in n:
+#         if(i%2!=0):
+#          return(i)
+
+
+
+# listdeki butun elementleri toplamaq ucun funksiya
+# def toplama(n):
+#     cem=0
+#     for i in n:
+#         cem=cem+i
+#     return(cem)
+
+# # Sample List : (8, 2, 3, 0, 7) Expected Output : 20
+# l=[8,2,3,0,7]
+# print(toplama(l))
+
+
+# Write a Python function to multiply all the numbers in a list.
+
+# def vurma(n):
+#     hasil=1
+#     for i in n:
+#         hasil=hasil*i
+#     return(hasil)
+
+# Sample List : (8, 2, 3, -1, 7) Expected Output : -336
+# l=[8,2,3,-1,7]
+# print(vurma(l))
+
